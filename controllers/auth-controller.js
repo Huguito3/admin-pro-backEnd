85f3ac29fc50150a98dcd0c945738bf02594ca6d
Hugo D. I <hduraniroz@gmail.com>
2020-08-23 20:12:37 -0300

    alteracoes googleSign in

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -47,11 +47,32 @@ const login = async (req, res = response) => {
 const googleSignIn = async (req, res = response) => {
   const googleToken = req.body.token;
   try {
-    const {name, email, picture} = await googleVerify(googleToken);
+    const { name, email, picture } = await googleVerify(googleToken);
+    const usuarioDB = await Usuario.findOne({ email });
+    let usuario;
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: "@@@",
+        img: picture,
+        google: true,
+      });
+    } else {
+      // si entro aqui es porque existe el usuario
+      usuario = usuarioDB;
+      usuario.google = true;
+      // esto es apra sobreescribir al contrasena y que solo use el login de google. Si se intenta loga con email y @@@ no funciona
+      usuario.password = "@@@";
+    }
+
+    await usuario.save();
+    // generamos el JWT de nuestra aplicacion
+
+    const token = await generarJWT(usuario.id);
     res.json({
       ok: true,
-      msg: "Google Sign In",
-      name, email, picture
+      token,
     });
   } catch (error) {
     res.status(401).json({
