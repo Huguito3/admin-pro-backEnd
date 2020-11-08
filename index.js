527aa4a7d233f7f894039cc8754a720bcdb910fa
Hugo D. I <hduraniroz@gmail.com>
2020-11-08 14:39:13 -0300

    alteracion del path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const { dbConnection } = require("./database/config");
 
 //Crear el servidor express
@@ -17,7 +17,7 @@ dbConnection();
 // console.log(process.env.PORT)
 
 // Directorio Publico. Ao executar a url no navegodr veremos nosso index html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Rutas
 //http://localhost:3000/
@@ -35,6 +35,13 @@ app.use("/api/medicos", require("./routes/medico-routes"));
 app.use("/api/todo", require("./routes/busquedas-routes"));
 app.use("/api/upload", require("./routes/upload-route"));
 app.use("/api/login", require("./routes/auth-routes"));
+
+// Si no es ningua de las anteriores rutas(esto lo hacemos porque incluimos en el public nuestra app angular)
+// si no hacemos lo de abajo cuando damos F5 en nuestra app angular no encuentra las rutas, por eso lo forzamos a
+// pasar por el index
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor corriendo en puerto: " + 3000);
